0d6c411f068370bcd9668945902641f80fa8c8ea
VitorDLeo <vitordileo003@gmail.com>
2022-05-20 11:35:20 -0300

    Conceitos em JS

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+import { RepositoryItem } from "./RepositoryItem"
+
+const repository = {
+    name: "unform",
+    description: "Forms in react",
+    link: "https://github.com/VitorDLeo/ReactJS"
+}
+
+export function RepositoryList(){
+    return(
+        <section className="repository-list">
+            <h1>Lista de repositorio</h1>
+
+            <ul>
+                <RepositoryItem repository={repository} />
+
+                <RepositoryItem repository={repository} />
+
+                <RepositoryItem repository={repository} />
+
+                <RepositoryItem repository={repository} />
+            </ul>
+        </section>
+    )
+}
